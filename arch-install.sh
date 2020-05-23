f56cef87e5b42e2cec767060840be6c93fb36d20
Andrey Elagin <daddyingrave@gmail.com>
2020-05-24 00:51:35 +0300

    implement date/time, lang and general packages install

--- arch-install.sh
+++ arch-install.sh
@@ -1,31 +1,35 @@
+#!/usr/bin/env bash
 
+echo "Setup date/time"
 
-#Installation:
-#
-#1. cfdisk
-#2.
-#3. mount efi?
-#4. mount swap?
-#5. mount /dev/??? /mnt
-#6. timedatectl set-ntp true
-#7. pacstrap /mnt base base-devel linux linux-firmware openssh
-#8. genfstab -U /mnt >> /mnt/etc/fstab
-#9. ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-#10. hwclock --systohc
-#11.  Generate locale
-#
-#    ```bash
-#    mv /etc/locale.gen /etc/locale.gen.bak && \
-#    echo “en_US.UTF-8 UTF-8” >> /etc/locale.gen && \
-#    echo “ru_RU.UTF-8 UTF-8” >> /etc/locale.gen && \
-#    echo “LANG=en_US.UTF-8” > /etc/locale.conf && \
-#    cat /etc/locale.gen && \
-#    cat /etc/locale.conf && \
-#    locale-gen
-#    ```
-#
-#12. echo LANG=en_US.UTF-8 >> /etc/locale.conf
-#13. echo daddyingrave >> /etc/hostname
+timedatectl set-ntp true
+
+echo "Installing OS packages"
+
+pacstrap /mnt base base-devel linux linux-firmware openssh dhcpcd grub
+
+echo "Filling fstab"
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Setting up timezon"
+
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
+
+echo "Setting up locales"
+
+mv /etc/locale.gen /etc/locale.gen.bak && \
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen && \
+echo "LANG=en_US.UTF-8" > /etc/locale.conf && \
+cat /etc/locale.gen && \
+cat /etc/locale.conf && \
+locale-gen
+
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+
+#echo daddyingrave >> /etc/hostname
 #14. pacman -S dhcpcd
 #15. systemctl enable sshd.service && systemctl enable dhcpcd
 #16. echo -e '127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\tdaddyingrave.localdomain\tdaddyingrave' /etc/hosts
